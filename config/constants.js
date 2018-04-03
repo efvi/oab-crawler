755942ae726029d7c418964f392d6267bdb9ef2a
Eduardo Vieira <eduardofelipevieira@gmail.com>
2018-04-03 13:56:41 -0300

    Commit

--- config/constants.js
+++ config/constants.js
@@ -1,6 +1,6 @@
 'use strict'
 
 module.exports = {
-    pdfDestination: '/Users/edu/Desktop/oab7',
+    pdfDestination: '/Users/digicon/Desktop/oab7',
     originUrl: 'https://www.qconcursos.com/questoes-da-oab/provas/search?&organizadora=Banca&ano=Ano&orgao_hidden=6152+&banca_hidden=63+&ano_hidden=%d&page=%d'
 }
